c0a47a28e6cc9ba609af02b3eaec741ff98e9333
Tony <4306632@qq.com>
2015-02-14 20:42:56 -0600

    Add STRATEGY_TYPE enum.

---
+++ Workspace/ElevatorSimulation/src/se450/elevator/common/STRATEGY_TYPE.java
@@ -0,0 +1,14 @@
+package se450.elevator.common;
+
+
+/**
+ * The strategies for elevator system:
+ * 1) LEAST_WAITING_TIME
+ * 2) LEAST_ELEVATOR_MOVEMENT 
+ * @author Cheng Zhang
+ *
+ */
+public enum STRATEGY_TYPE {
+	LEAST_WAITING_TIME,
+	LEAST_ELEVATOR_MOVEMENT
+}
